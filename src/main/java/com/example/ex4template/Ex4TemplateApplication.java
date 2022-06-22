0cb64dda16de8c16913362856e63512e935826fe
david nyssen <76433487+davidnyseen@users.noreply.github.com>
2022-06-22 18:22:28 +0300

    Initial commit

---
+++ src/main/java/com/example/ex4template/Ex4TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex4template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex4TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex4TemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ex4template/Ex4TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex4template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex4TemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
